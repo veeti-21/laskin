33bfbcef1a44eacaa293c7706b28edd6fcc9d773
veeti-21 <158137597+veeti-21@users.noreply.github.com>
2025-08-27 09:10:17 +0300

    laskimen pohja
    laskimen pohja ilman laskuja

---
+++ laskin/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("minkä laskun haluat tehdä summa = 1 vähennys = 2 kerto = 3 jako = 4. ");
+int lasku = int.Parse(Console.ReadLine());
+Console.WriteLine("luku 1: ");
+int luku1 = int.Parse(Console.ReadLine());
+Console.WriteLine("luku 2: ");
+int luku2 = int.Parse(Console.ReadLine());
+
+switch (lasku) {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+
+}
+
