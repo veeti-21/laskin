2aa8efd65177f06770ac04069a685e932a0a2d03
katisaara <katike.kemppainen@gmail.com>
2025-08-27 09:30:54 +0300

    Update Program.cs

--- laskin/Program.cs
+++ laskin/Program.cs
@@ -1,13 +1,13 @@
 ﻿Console.WriteLine("minkä laskun haluat tehdä summa = 1 vähennys = 2 kerto = 3 jako = 4. ");
-int lasku = int.Parse(Console.ReadLine());
+double lasku = double.Parse(Console.ReadLine());
 Console.WriteLine("luku 1: ");
-int luku1 = int.Parse(Console.ReadLine());
+double luku1 = double.Parse(Console.ReadLine());
 Console.WriteLine("luku 2: ");
-int luku2 = int.Parse(Console.ReadLine());
+double luku2 = double.Parse(Console.ReadLine());
 
 switch (lasku) {
     case 1:
-        int summa = luku1 + luku2;
+        double summa = luku1 + luku2;
         Console.WriteLine("Summa on: " + summa);            // Käyttäjä syöttää luvun 1 ja 2, sitten laskin laskee ne yhteen ja tulostaa lopputuloksen.
         break;
 
