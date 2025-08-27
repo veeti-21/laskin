f982e72a5edaaf0a7d631bf5690937a522cec691
katisaara <katike.kemppainen@gmail.com>
2025-08-27 09:26:29 +0300

    Update Program.cs
    yhteenlasku tehty

--- laskin/Program.cs
+++ laskin/Program.cs
@@ -7,6 +7,10 @@ int luku2 = int.Parse(Console.ReadLine());
 
 switch (lasku) {
     case 1:
+        int summa = luku1 + luku2;
+        Console.WriteLine("Summa on: " + summa);            // Käyttäjä syöttää luvun 1 ja 2, sitten laskin laskee ne yhteen ja tulostaa lopputuloksen.
+        break;
+
         break;
     case 2:
         break;
